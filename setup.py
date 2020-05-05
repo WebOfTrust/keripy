356bbcb1cef0feed15729a8969c1b89a628d39c3
Samuel M Smith <smith.samuel.m@gmail.com>
2020-05-05 17:03:19 -0600

    setting up

--- setup.py
+++ setup.py
@@ -6,17 +6,11 @@ $ python setup.py register sdist upload
 More secure to use twine to upload
 $ pip3 install twine
 $ python3 setup.py sdist
-$ twine upload dist/leopy-0.1.2.tar.gz
+$ twine upload dist/keri-0.0.1.tar.gz
 
 """
 
 
-
-from __future__ import absolute_import
-from __future__ import print_function
-
-import io
-import re
 from glob import glob
 from os.path import basename
 from os.path import dirname
@@ -27,23 +21,16 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-def read(*names, **kwargs):
-    with io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
-    ) as fh:
-        return fh.read()
-
 
 setup(
-    name='leopy',
-    version='0.1.6',
+    name='keri',
+    version='0.0.1',
     license='Apache Software License 2.0',
-    description='Hyperledger Aries Cloud Agent Controller Demo',
-    long_description="Hyperledger Aries Cloud Agent Controller Demo",
+    description='Key Event Receipt Infrastructure',
+    long_description="KERI Decentralized Key Management Infrastructure",
     author='Samuel M. Smith',
     author_email='smith.samuel.m@gmail.com',
-    url='https://github.com/SmithSamuelM/leopy',
+    url='https://github.com/SmithSamuelM/keri',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
@@ -51,13 +38,13 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: Unix',
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         # uncomment if you test on these interpreters:
         #'Programming Language :: Python :: Implementation :: PyPy',
@@ -67,14 +54,14 @@ setup(
         'Topic :: Utilities',
     ],
     project_urls={
-        'Documentation': 'https://leopy.readthedocs.io/',
-        'Changelog': 'https://leopy.readthedocs.io/en/latest/changelog.html',
-        'Issue Tracker': 'https://github.com/SmithSamuelM/leopy/issues',
+        'Documentation': 'https://keri.readthedocs.io/',
+        'Changelog': 'https://keri.readthedocs.io/en/latest/changelog.html',
+        'Issue Tracker': 'https://github.com/SmithSamuelM/keri/issues',
     },
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[
         'ioflo>=1.7.6',
         'falcon>=2.0.0',
@@ -88,8 +75,8 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'leopy = leopy.cli:main',
-            'leopyd = leopy.daemon:main'
+            'keri = keri.cli:main',
+            'kerid = keri.daemon:main'
         ]
     },
 )
