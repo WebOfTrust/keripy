3f8e03b242891a8167095d1b7da578823aab50e5
Samuel M Smith <smith.samuel.m@gmail.com>
2020-05-06 12:00:42 -0600

    setting stuff up

--- src/keri/help/helping.py
+++ src/keri/help/helping.py
@@ -39,3 +39,20 @@ def cleanupTmpBaseDir(baseDirPath):
                 break
             baseDirPath = os.path.dirname(baseDirPath)
 
+
+import pytest
+
+@pytest.fixture(autouse=True)
+def setupTeardown():
+    """
+    Pytest runs this function before every test when autouse=True
+    Without autouse=True you would have to add a setupTeardown parameter
+    to each test function
+    """
+    #setup
+    DB_DIR_PATH = "/tmp/db_setup_test"
+    yield DB_DIR_PATH  # this allows the test to run
+
+    # teardown
+    cleanupBaseDir(DB_DIR_PATH)
+    assert not os.path.exists(DB_DIR_PATH)
