2a704b608c9edbabca8d350864f268b062c30bdc
Samuel M Smith <smith.samuel.m@gmail.com>
2020-07-30 14:05:55 -0600

    added helper class mdict as MultiDict but with support for LIFO

--- src/keri/help/helping.py
+++ src/keri/help/helping.py
@@ -10,6 +10,10 @@ import base64
 
 import pysodium
 
+from multidict import MultiDict  # base class for mdict defined below
+from orderedset import OrderedSet as oset
+
+
 def cleanupBaseDir(baseDirPath):
     """
     Remove baseDirPath
@@ -84,3 +88,103 @@ def verify64uEd25519(signature, message, verkey):
     msg = message.encode("utf-8")
     return (verifyEd25519(sig, msg, vk))
 
+class mdict(MultiDict):
+    """
+    Multiple valued dictionary. Insertion order of keys preserved.
+    Associated with each key is a valuelist i.e. a list of values for that key.
+    Extends  MultiDict
+    https://multidict.readthedocs.io/en/stable/
+
+    In MultiDict:
+        .add(key,value)  appends value to the valuelist at key
+
+        m["key"] = value replaces the valuelist at key with [value]
+
+        m["key] treturns the first added element of the valuelist at key
+
+    MultiDict methods access values in FIFO order
+    mdict adds method to access values in LIFO order
+
+    Extended methods in mdict but not in MultiDict are:
+       nabone(key [,default])  get last value at key else default or KeyError
+       nab(key [,default])  get last value at key else default or None
+       naball(key [,default]) get all values inverse order else default or KeyError
+
+    """
+
+    def nabone(self, key, *pa, **kwa):
+        """
+        Usage:
+            .nabone(key [, default])
+
+        returns last value at key if key in dict else default
+        raises KeyError if key not in dict and default not provided.
+        """
+        try:
+            return self.getall(key)[-1]
+        except KeyError:
+            if not pa and "default" not in kwa:
+                raise
+            elif pa:
+                return pa[0]
+            else:
+                return kwa["default"]
+
+
+    def nab(self, key, *pa, **kwa):
+        """
+        Usage:
+            .nab(key [, default])
+
+        returns last value at key if key in dict else default
+        returns None if key not in dict and default not provided.
+        """
+        try:
+            return self.getall(key)[-1]
+        except KeyError:
+            if not pa and "default" not in kwa:
+                return None
+            elif pa:
+                return pa[0]
+            else:
+                return kwa["default"]
+
+    def naball(self, key, *pa, **kwa):
+        """
+        Usage:
+            .nabone(key [, default])
+
+        returns list of value at key if key in dict else default
+        raises KeyError if key not in dict and default not provided.
+        """
+        try:
+            # getall returns copy of list so safe to reverse
+            return list(reversed(self.getall(key)))
+        except KeyError:
+            if not pa and "default" not in kwa:
+                raise
+            elif pa:
+                return pa[0]
+            else:
+                return kwa["default"]
+
+
+    def firsts(self):
+        """
+        Returns list of (key, value) pair where each value is first value at key
+        No duplicate keys
+
+        This is useful for forked lists of values with same keys
+        """
+        keys = oset(self.keys())  # get rid of duplicates provided by .keys()
+        return [(k, self.getone(k)) for k in keys]
+
+
+    def lasts(self):
+        """
+        Returns list of (key, value) pairs where each value is last value at key
+
+        This is useful fo forked lists  of values with same keys
+        """
+        keys = oset(self.keys())  # get rid of duplicates provided by .keys()
+        return [(k, self.nabone(k)) for k in keys]
--- tests/help/test_helping.py
+++ tests/help/test_helping.py
@@ -3,10 +3,67 @@
 tests.help.test_helping module
 
 """
+import pytest
 
 import pysodium
+
+
+from keri.help.helping import mdict
+
+
+def test_mdict():
+    """
+    Test mdict multiple value dict
+    """
+    from multidict import MultiDict
+    from collections.abc import MutableMapping
+
+    m = mdict()
+    assert isinstance(m, MultiDict)
+    assert isinstance(m, MutableMapping)
+    assert not isinstance(m, dict)
+
+    m = mdict(a=1, b=2, c=3)
+
+    m.add("a", 7)
+    m.add("b", 8)
+    m.add("c", 9)
+
+    assert m.getone("a") == 1
+    assert m.nabone("a") == 7
+
+    assert m.get("a") == 1
+    assert m.nab("a") == 7
+
+    assert m.getall("a") == [1, 7]
+    assert m.naball("a") == [7, 1]
+
+    assert m.nabone("z", 5) == 5
+    with pytest.raises(KeyError):
+        m.nabone("z")
+
+    assert m.nab("z", 5) == 5
+    assert m.nab("z") == None
+
+    assert m.naball("z", []) == []
+    with pytest.raises(KeyError):
+        m.naball("z")
+
+    assert list(m.keys()) == ['a', 'b', 'c', 'a', 'b', 'c']
+
+    assert m.firsts() == [('a', 1), ('b', 2), ('c', 3)]
+
+    assert m.lasts() == [('a', 7), ('b', 8), ('c', 9)]
+
+    """End Test"""
 
 def test_conversions():
     """
     Test conversion helpers
     """
+
+
+
+
+if __name__ == "__main__":
+    test_mdict()
