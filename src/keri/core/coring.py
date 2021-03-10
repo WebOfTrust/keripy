13898c8a543556b12b8867592a46a532c90db93e
Samuel M Smith <smith.samuel.m@gmail.com>
2021-03-09 18:16:58 -0700

    added new Dater class for DateTime attachments with tests.

--- src/keri/core/coring.py
+++ src/keri/core/coring.py
@@ -26,7 +26,7 @@ from ..kering import (ValidationError, VersionError, EmptyMaterialError,
                       DerivationError, ShortageError,  DerivationCodeError,
                       UnexpectedCountCodeError, UnexpectedOpCodeError)
 from ..kering import Versionage, Version
-from ..help.helping import sceil
+from ..help.helping import sceil, nowIso8601
 
 Serialage = namedtuple("Serialage", 'json mgpk cbor')
 
@@ -818,6 +818,96 @@ class Seqner(Matter):
         Returns .raw converted to hex str
         """
         return "{:x}".format(self.sn)
+
+
+class Dater(Matter):
+    """
+    Dater is subclass of Matter, cryptographic material, for ISO-8601 datetimes.
+    Dater provides a custom Base64 coding of an ASCII ISO-8601 datetime by replacing
+    the three non-Base64 characters ':.+' with the Base64 equivalents 'cdp'.
+    Dater provides a more compact representation than would be obtained by converting
+    the raw ASCII ISO-8601 datetime to Base64.
+    Dater supports datetimes as attached crypto material in replay of events for
+    the datetime of when the event was first seen.
+    Restricted to specific 32 byte variant of ISO-8601 date time with microseconds
+    and UTC offset in HH:MM. For example:
+
+    '2020-08-22T17:50:09.988921+00:00'
+    '2020-08-22T17:50:09.988921-01:00'
+
+    The fully encoded versions are respectively
+
+    '1AAG2020-08-22T17c50c09d988921p00c00'
+    '1AAG2020-08-22T17c50c09d988921-01c00'
+
+    Useful when parsing attached first seen couples with fn  + dt
+
+    Uses default initialization code = MtrDex.DateTime
+    Raises error on init if code not  MtrDex.DateTime
+
+    Attributes:
+
+    Inherited Properties:  (See Matter)
+        .pad  is int number of pad chars given raw
+        .code is  str derivation code to indicate cypher suite
+        .raw is bytes crypto material only without code
+        .index is int count of attached crypto material by context (receipts)
+        .qb64 is str in Base64 fully qualified with derivation code + crypto mat
+        .qb64b is bytes in Base64 fully qualified with derivation code + crypto mat
+        .qb2  is bytes in binary with derivation code + crypto material
+        .nontrans is Boolean, True when non-transferable derivation code False otherwise
+
+    Properties:
+        .dt is the ISO-8601 datetime
+
+    Hidden:
+        ._pad is method to compute  .pad property
+        ._code is str value for .code property
+        ._raw is bytes value for .raw property
+        ._index is int value for .index property
+        ._infil is method to compute fully qualified Base64 from .raw and .code
+        ._exfil is method to extract .code and .raw from fully qualified Base64
+
+    Methods:
+
+    """
+    ToB64 = str.maketrans(":.+", "cdp")
+    FromB64 = str.maketrans("cdp", ":.+")
+
+    def __init__(self, raw=None, qb64b=None, qb64=None, qb2=None,
+                 code=MtrDex.Salt_128, dt=None, **kwa):
+        """
+        Inhereited Parameters:  (see Matter)
+            raw is bytes of unqualified crypto material usable for crypto operations
+            qb64b is bytes of fully qualified crypto material
+            qb64 is str or bytes  of fully qualified crypto material
+            qb2 is bytes of fully qualified crypto material
+            code is str of derivation code
+            index is int of count of attached receipts for CryCntDex codes
+
+        Parameters:
+            dt the ISO-8601 datetime
+        """
+        if raw is None and qb64b is None and qb64 is None and qb2 is None:
+            if dt is None:  # defaults to now
+                dt = nowIso8601()
+            if len(dt) != 32:
+                raise ValueError("Invalid length of date time string")
+            qb64 = MtrDex.DateTime + dt.translate(self.ToB64)
+
+        super(Dater, self).__init__(raw=raw, qb64b=qb64b, qb64=qb64, qb2=qb2,
+                                         code=code, **kwa)
+        if self.code != MtrDex.DateTime:
+            raise ValidationError("Invalid code = {} for Dater date time."
+                                  "".format(self.code))
+
+    @property
+    def dt(self):
+        """
+        Property sn:
+        Returns .raw converted to int
+        """
+        return self.qb64[self.Codes[self.code].hs:].translate(self.FromB64)
 
 
 class Verfer(Matter):
