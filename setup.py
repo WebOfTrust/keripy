37c29e84d36e6bff90ebfb9eab486227f2b3d3ba
Samuel M Smith <smith.samuel.m@gmail.com>
2021-05-08 17:06:14 -0600

    updated to support latest hio

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from setuptools import setup
 
 setup(
     name='keri',
-    version='0.4.2',  #  also change in src/keri/__init__.py
+    version='0.4.3',  #  also change in src/keri/__init__.py
     license='Apache Software License 2.0',
     description='Key Event Receipt Infrastructure',
     long_description="KERI Decentralized Key Management Infrastructure",
@@ -82,7 +82,7 @@ setup(
         'cbor2>=5.2.0',
         'multidict>=5.1.0',
         'orderedset>=2.0.3',
-        'hio>=0.3.3',
+        'hio>=0.3.5',
 
     ],
     extras_require={
