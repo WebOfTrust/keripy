68b301d9d6e7bc880339433bd6fa44848f83de5e
Philip Feairheller <pfeairheller@gmail.com>
2021-05-08 23:02:48 -0400

    Database implementation and events for credential registry TEL (#140)
    * TEL Sub-databases: implementation and unit tests
    
    * Utility functions for creating bare TEL events for credential registry.
    
    Signed-off-by: Phil Feairheller <pfeairheller@gmail.com>

--- src/keri/vdr/viring.py
+++ src/keri/vdr/viring.py
@@ -4,24 +4,60 @@ keri.db.viring module
 
 VIR  Verifiable Issuance(Revocation) Registry
 
-Provides public simple Verificable Credential Issuance/Revocation Registry
+Provides public simple Verifiable Credential Issuance/Revocation Registry
 A special purpose Verifiable Data Registry (VDR)
 """
 
 from keri.db import dbing
 
-class Issuer(dbing.LMDBer):
+
+class Registry(dbing.LMDBer):
     """
     Issuer sets up named sub databases for VIR
 
     Attributes:
         see superclass LMDBer for inherited attributes
 
-        .virs is named sub DB whose values are VC issuance/revocation state
+
+        .tvts is named sub DB whose values are serialized TEL events
             dgKey
             DB is keyed by identifer prefix plus digest of serialized event
             Only one value per DB key is allowed
-
+        .tels is named sub DB of transaction event log tables that map sequence numbers
+            to serialized event digests.
+            snKey
+            Values are digests used to lookup event in .tvts sub DB
+            DB is keyed by identifer prefix plus sequence number of tel event
+            Only one value per DB key is allowed
+        .tibs is named sub DB of indexed backer signatures of event
+            Backers always have nontransferable indetifier prefixes.
+            The index is the offset of the backer into the backer list
+            of the anchored management event wrt the receipted event.
+            dgKey
+            DB is keyed by identifer prefix plus digest of serialized event
+            More than one value per DB key is allowed
+        .oots is named sub DB of out of order escrowed event tables
+            that map sequence numbers to serialized event digests.
+            snKey
+            Values are digests used to lookup event in .tvts sub DB
+            DB is keyed by identifer prefix plus sequence number of key event
+            Only one value per DB key is allowed
+        .twes is named sub DB of partially witnessed escrowed event tables
+            that map sequence numbers to serialized event digests.
+            snKey
+            Values are digests used to lookup event in .tvts sub DB
+            DB is keyed by identifer prefix plus sequence number of tel event
+            Only one value per DB key is allowed
+        .ancs is a named sub DB of anchors to KEL events.  Quadlet
+            Each quadruple is concatenation of  four fully qualified items
+            of validator. These are: transferable prefix, plus latest establishment
+            event sequence number plus latest establishment event digest,
+            plus indexed event signature.
+            When latest establishment event is multisig then there will
+            be multiple quadruples one per signing key, each a dup at same db key.
+            dgKey
+            DB is keyed by identifer prefix plus digest of serialized event
+            Only one value per DB key is allowed
 
     Properties:
 
@@ -55,18 +91,264 @@ class Issuer(dbing.LMDBer):
         Duplicates are inserted in lexocographic order by value, insertion order.
 
         """
-        super(Issuer, self).__init__(headDirPath=headDirPath, reopen=reopen, **kwa)
-
+        super(Registry, self).__init__(headDirPath=headDirPath, reopen=reopen, **kwa)
 
     def reopen(self, **kwa):
         """
         Open sub databases
         """
-        super(Issuer, self).reopen(**kwa)
+        super(Registry, self).reopen(**kwa)
 
         # Create by opening first time named sub DBs within main DB instance
         # Names end with "." as sub DB name must include a non Base64 character
         # to avoid namespace collisions with Base64 identifier prefixes.
 
-        self.virs = self.env.open_db(key=b'virs.')
+        self.tvts = self.env.open_db(key=b'tvts.')
+        self.tels = self.env.open_db(key=b'tels.')
+        self.tibs = self.env.open_db(key=b'tibs.', dupsort=True)
+        self.oots = self.env.open_db(key=b'oots.')
+        self.twes = self.env.open_db(key=b'twes.')
+        self.ancs = self.env.open_db(key=b'ancs.',)
+
+    def putTvt(self, key, val):
+        """
+        Use dgKey()
+        Write serialized VC bytes val to key
+        Does not overwrite existing val if any
+        Returns True If val successfully written Else False
+        Return False if key already exists
+        """
+        return self.putVal(self.tvts, key, val)
+
+    def setTvt(self, key, val):
+        """
+        Use dgKey()
+        Write serialized VC bytes val to key
+        Overwrites existing val if any
+        Returns True If val successfully written Else False
+        """
+        return self.setVal(self.tvts, key, val)
+
+    def getTvt(self, key):
+        """
+        Use dgKey()
+        Return event at key
+        Returns None if no entry at key
+        """
+        return self.getVal(self.tvts, key)
+
+    def delTvt(self, key):
+        """
+        Use dgKey()
+        Deletes value at key.
+        Returns True If key exists in database Else False
+        """
+        return self.delVal(self.tvts, key)
+
+    def putTel(self, key, val):
+        """
+        Use dgKey()
+        Write serialized VC bytes val to key
+        Does not overwrite existing val if any
+        Returns True If val successfully written Else False
+        Return False if key already exists
+        """
+        return self.putVal(self.tels, key, val)
+
+    def setTel(self, key, val):
+        """
+        Use snKey()
+        Write serialized VC bytes val to key
+        Overwrites existing val if any
+        Returns True If val successfully written Else False
+        """
+        return self.setVal(self.tels, key, val)
+
+    def getTel(self, key):
+        """
+        Use snKey()
+        Return event at key
+        Returns None if no entry at key
+        """
+        return self.getVal(self.tels, key)
+
+    def delTel(self, key):
+        """
+        Use snKey()
+        Deletes value at key.
+        Returns True If key exists in database Else False
+        """
+        return self.delVal(self.tels, key)
+
+    def getTibs(self, key):
+        """
+        Use dgKey()
+        Return list of indexed witness signatures at key
+        Returns empty list if no entry at key
+        Duplicates are retrieved in lexocographic order not insertion order.
+        """
+        return self.getVals(self.tibs, key)
+
+    def getTibsIter(self, key):
+        """
+        Use dgKey()
+        Return iterator of indexed witness signatures at key
+        Raises StopIteration Error when empty
+        Duplicates are retrieved in lexocographic order not insertion order.
+        """
+        return self.getValsIter(self.tibs, key)
+
+    def putTibs(self, key, vals):
+        """
+        Use dgKey()
+        Write each entry from list of bytes indexed witness signatures vals to key
+        Adds to existing signatures at key if any
+        Returns True If no error
+        Apparently always returns True (is this how .put works with dupsort=True)
+        Duplicates are inserted in lexocographic order not insertion order.
+        """
+        return self.putVals(self.tibs, key, vals)
+
+    def addTib(self, key, val):
+        """
+        Use dgKey()
+        Add indexed witness signature val bytes as dup to key in db
+        Adds to existing values at key if any
+        Returns True if written else False if dup val already exists
+        Duplicates are inserted in lexocographic order not insertion order.
+        """
+        return self.addVal(self.tibs, key, val)
+
+    def cntTibs(self, key):
+        """
+        Use dgKey()
+        Return count of indexed witness signatures at key
+        Returns zero if no entry at key
+        """
+        return self.cntVals(self.tibs, key)
+
+    def delTibs(self, key, val=b''):
+        """
+        Use dgKey()
+        Deletes all values at key if val = b'' else deletes dup val = val.
+        Returns True If key exists in database (or key, val if val not b'') Else False
+        """
+        return self.delVals(self.tibs, key, val)
+
+    def putTwe(self, key, val):
+        """
+        Use snKey()
+        Write serialized VC bytes val to key
+        Does not overwrite existing val if any
+        Returns True If val successfully written Else False
+        Return False if key already exists
+        """
+        return self.putVal(self.twes, key, val)
+
+    def setTwe(self, key, val):
+        """
+        Use snKey()
+        Write serialized VC bytes val to key
+        Overwrites existing val if any
+        Returns True If val successfully written Else False
+        """
+        return self.setVal(self.twes, key, val)
+
+    def getTwe(self, key):
+        """
+        Use snKey()
+        Return event at key
+        Returns None if no entry at key
+        """
+        return self.getVal(self.twes, key)
+
+    def delTwe(self, key):
+        """
+        Use snKey()
+        Deletes value at key.
+        Returns True If key exists in database Else False
+        """
+        return self.delVal(self.twes, key)
+
+
+    def putOot(self, key, val):
+        """
+        Use snKey()
+        Write serialized VC bytes val to key
+        Does not overwrite existing val if any
+        Returns True If val successfully written Else False
+        Return False if key already exists
+        """
+        return self.putVal(self.oots, key, val)
+
+    def setOot(self, key, val):
+        """
+        Use snKey()
+        Write serialized VC bytes val to key
+        Overwrites existing val if any
+        Returns True If val successfully written Else False
+        """
+        return self.setVal(self.oots, key, val)
+
+    def getOot(self, key):
+        """
+        Use snKey()
+        Return event at key
+        Returns None if no entry at key
+        """
+        return self.getVal(self.oots, key)
+
+    def delOot(self, key):
+        """
+        Use snKey()
+        Deletes value at key.
+        Returns True If key exists in database Else False
+        """
+        return self.delVal(self.oots, key)
+
+
+    def putAnc(self, key, val):
+        """
+        Use snKey()
+        Write serialized VC bytes val to key
+        Does not overwrite existing val if any
+        Returns True If val successfully written Else False
+        Return False if key already exists
+        """
+        return self.putVal(self.ancs, key, val)
+
+    def setAnc(self, key, val):
+        """
+        Use snKey()
+        Write serialized VC bytes val to key
+        Overwrites existing val if any
+        Returns True If val successfully written Else False
+        """
+        return self.setVal(self.ancs, key, val)
+
+    def getAnc(self, key):
+        """
+        Use snKey()
+        Return event at key
+        Returns None if no entry at key
+        """
+        return self.getVal(self.ancs, key)
+
+    def delAnc(self, key):
+        """
+        Use snKey()
+        Deletes value at key.
+        Returns True If key exists in database Else False
+        """
+        return self.delVal(self.ancs, key)
+
+
+def nsKey(comps):
+    """
+    Returns bytes namespaced key from concatenation of ':' with qualified Base64
+    prefix bytes components
+    If any component is a str then converts to bytes
+    """
+    comps = map(lambda p: p if not hasattr(p, "encode") else p.encode("utf-8"), comps)
+    return b':'.join(comps)
 
