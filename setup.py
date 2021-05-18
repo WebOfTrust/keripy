6aa79a5ffc1da00775555c62025aca829eb19e48
Samuel M Smith <smith.samuel.m@gmail.com>
2021-05-18 14:18:23 -0600

    bump version for omnibus part 1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from setuptools import setup
 
 setup(
     name='keri',
-    version='0.4.3',  # also change in src/keri/__init__.py
+    version='0.4.4',  # also change in src/keri/__init__.py
     license='Apache Software License 2.0',
     description='Key Event Receipt Infrastructure',
     long_description="KERI Decentralized Key Management Infrastructure",
