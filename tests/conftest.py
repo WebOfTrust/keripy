b8ab9c26e51e49aabc07536d9c7d2a1f68ba65b1
Kevin Griffin <griffin.kev@gmail.com>
2021-06-01 11:06:14 -0400

    add date time stamps to iss/bis/rev/brv messages (#185)

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,16 @@ Use this module to configure pytest
 https://docs.pytest.org/en/latest/pythonpath.html
 
 """
-import os
 import pytest
 
+from keri.help import helping
+
+
+@pytest.fixture()
+def mockHelpingNowIso8601(monkeypatch):
+    """keri.help.helping.nowIso8601() mocked to return 2021-05-30T17:42:26.716070+00:00"""
+
+    def mock_nowIso8601():
+        return "2021-05-30T17:42:26.716070+00:00"
+
+    monkeypatch.setattr(helping, "nowIso8601", mock_nowIso8601)
