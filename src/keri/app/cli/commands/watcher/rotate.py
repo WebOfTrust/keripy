c010880f04d17e5aadfaceaa579ac5fe664dbc93
Phil Feairheller <pfeairheller@gmail.com>
2021-08-27 08:46:59 -0400

    Delegation using delegation publish protocol to communicate between parties.
    Converted watcher rotate to KERI Web Auth'ed HTTP
    
    Signed-off-by: Phil Feairheller <pfeairheller@gmail.com>

--- src/keri/app/cli/commands/watcher/rotate.py
+++ src/keri/app/cli/commands/watcher/rotate.py
@@ -4,17 +4,12 @@ keri.kli.commands.watcher module
 
 """
 import argparse
-import json
 
 from hio.base import doing
-from hio.core.http import clienting
-from hio.help import decking
 
-from keri import help, kering
-from keri.app import directing, obtaining
+from keri import help
+from keri.app import directing, watching
 from keri.app.cli.common import existing
-from keri.core import eventing, parsing, coring
-from keri.end import ending
 
 parser = argparse.ArgumentParser(description='Rotate watcher prefix')
 parser.set_defaults(handler=lambda args: rotateWatcher(args))
@@ -28,70 +23,32 @@ def rotateWatcher(args):
     name = args.name
     wat = args.watcher
 
-    rotr = WatcherRotateDoer(name=name, watcher=wat)
-    directing.runController(doers=[rotr], expire=0.0)
+    watr = WatcherRotate(name=name, wat=wat)
+    directing.runController(doers=[watr], expire=0.0)
 
 
-class WatcherRotateDoer(doing.DoDoer):
+class WatcherRotate(doing.DoDoer):
 
+    def __init__(self, name, wat, **kwa):
+        self.watcher = wat
+        self.hab, doers = existing.openHabitat(name=name)
+        self.rotr = watching.WatcherClientRotateDoer(hab=self.hab)
+        doers.extend([self.rotr])
+        self.toRemove = list(doers)
 
-    def __init__(self, name, watcher, **kwa):
-        hab, doers = existing.openHabitat(name=name)
-        self.hab = hab
-        self.watcher = watcher
-        self.reps = decking.Deck()
-
-        self.toRemove = doers
         doers.extend([doing.doify(self.rotateDo)])
-        super(WatcherRotateDoer, self).__init__(doers=doers, **kwa)
+
+        super(WatcherRotate, self).__init__(doers=doers, **kwa)
 
 
     def rotateDo(self, tymth, tock=0.0, **opts):
-        """
-        Returns:  doifiable Doist compatible generator method
-        Usage:
-            add result of doify on this method to doers list
-        """
         # enter context
-        self.wind(tymth)
-        self.tock = tock
-        _ = (yield self.tock)
+        yield self.tock
 
-        habr = self.hab.db.habs.get(self.hab.name)
-        if self.watcher not in habr.watchers:
-            raise kering.ValidationError("identifier {} is not a current watcher {}"
-                                         "".format(self.watcher, habr.watchers))
+        self.rotr.msgs.append(self.watcher)
 
-
-        payload = dict(pre=self.hab.pre)
-        raw = json.dumps(payload)
-        sigers = self.hab.mgr.sign(ser=raw.encode("utf-8"),
-                                   verfers=self.hab.kever.verfers,
-                                   indexed=True)
-
-        signage = ending.Signage(markers=sigers, indexed=True)
-        headers = ending.signature([signage])
-
-        loc = obtaining.getwitnessbyprefix(self.watcher)
-        client = clienting.Client(hostname=loc.ip4, port=loc.http)
-        clientDoer = clienting.ClientDoer(client=client)
-        self.extend([clientDoer])
-        self.toRemove.extend([clientDoer])
-
-        client.request(method="POST", path="/rotate", headers=headers, body=raw)
-        while not client.responses:
+        while not self.rotr.cues:
             yield self.tock
-
-        resp = client.respond()
-        if resp.status != 200:
-            print("Invalid status from watcher:", type(resp.status))
-            return
-
-        if not self.authenticate(resp):
-            print("Invalid response from watcher")
-            return
-
-        self.processWatcherResponse(bytes(resp.body))
 
         habr = self.hab.db.habs.get(self.hab.name)
         print("New Watcher Set:")
@@ -99,33 +56,3 @@ class WatcherRotateDoer(doing.DoDoer):
             print("\t{}".format(wat))
 
         self.remove(self.toRemove)
-
-    def processWatcherResponse(self, icp):
-        ctrlKvy = eventing.Kevery(db=self.hab.db)
-        parsing.Parser().parse(ims=bytearray(icp), kvy=ctrlKvy)
-
-        srdr = coring.Serder(raw=bytearray(icp))
-        wat = srdr.pre
-
-        habr = self.hab.db.habs.get(self.hab.name)
-        ewats = set(habr.watchers)
-
-        ewats.remove(self.watcher)
-        ewats.add(wat)
-
-        habr.watchers = list(ewats)
-
-        self.hab.db.habs.pin(self.hab.name, habr)
-
-    def authenticate(self, resp):
-        if "Signature" not in resp.headers:
-            return False
-
-        signages = ending.designature(resp.headers["Signature"])
-
-        cigar = signages[0].markers[self.watcher]
-        verfer = coring.Verfer(qb64=self.watcher)
-        if not verfer.verify(cigar.raw, bytes(resp.body)):
-            return False
-
-        return True
--- src/keri/app/watching.py
+++ src/keri/app/watching.py
@@ -8,11 +8,12 @@ import json
 
 import falcon
 from hio.base import doing
+from hio.core.http import clienting
 from hio.help import decking
 
-from keri.app import keeping, forwarding
-from keri.core import coring, eventing
-from .. import help
+from keri.app import keeping, obtaining
+from keri.core import coring, eventing, parsing
+from .. import help, kering
 from ..end import ending
 
 logger = help.ogler.getLogger()
@@ -28,7 +29,7 @@ class KiwiServer(doing.DoDoer):
         self.hab = hab
         self.controller = controller
         self.app = app if app is not None else falcon.App(cors_enable=True)
-        app.add_middleware(SignatureValidationComponent(hab=self.hab, pre=self.controller))
+        self.app.add_middleware(SignatureValidationComponent(hab=self.hab, pre=self.controller))
         self.cues = cues if cues is not None else decking.Deck()
 
         self.app.add_route("/rotate", self, suffix="rotate")
@@ -138,5 +139,122 @@ class SignatureValidationComponent(object):
 
             if not verfer.verify(siger.raw, ser):
                 return False
+
+        return True
+
+
+class WatcherClientRotateDoer(doing.DoDoer):
+    """
+    Sends KERI Auth'ed HTTP request to specified watcher to rotate identifer.  Current
+    watcher set is then updated by removing the old watcher idenfitier and adding the
+    new watcher identifier.
+
+    """
+
+    def __init__(self, hab, msgs=None, cues=None, **kwa):
+        """
+        Create doer to rotate remote watcher identifier prefix
+
+        Parameters:
+            name: is str name of Habitat
+            watcher:
+            kwa:
+        """
+        self.hab = hab
+        self.msgs = msgs if msgs is not None else decking.Deck()
+        self.cues = cues if cues is not None else decking.Deck()
+
+        doers = [doing.doify(self.rotateDo)]
+        super(WatcherClientRotateDoer, self).__init__(doers=doers, **kwa)
+
+
+    def rotateDo(self, tymth, tock=0.0, **opts):
+        """
+        Returns:  doifiable Doist compatible generator method
+        Usage:
+            add result of doify on this method to doers list
+        """
+        # enter context
+        self.wind(tymth)
+        self.tock = tock
+        _ = (yield self.tock)
+
+        while True:
+            while self.msgs:
+                watcher = self.msgs.popleft()
+                habr = self.hab.db.habs.get(self.hab.name)
+                if watcher not in habr.watchers:
+                    raise kering.ValidationError("identifier {} is not a current watcher {}"
+                                                 "".format(watcher, habr.watchers))
+
+
+                payload = dict(pre=self.hab.pre)
+                raw = json.dumps(payload)
+                sigers = self.hab.mgr.sign(ser=raw.encode("utf-8"),
+                                           verfers=self.hab.kever.verfers,
+                                           indexed=True)
+
+                signage = ending.Signage(markers=sigers, indexed=True)
+                headers = ending.signature([signage])
+
+                loc = obtaining.getwitnessbyprefix(watcher)
+                client = clienting.Client(hostname=loc.ip4, port=loc.http)
+                clientDoer = clienting.ClientDoer(client=client)
+                self.extend([clientDoer])
+
+                client.request(method="POST", path="/rotate", headers=headers, body=raw)
+                while not client.responses:
+                    yield self.tock
+
+                resp = client.respond()
+                if resp.status != 200:
+                    print("Invalid status from watcher:", type(resp.status))
+                    return
+
+                if not self.authenticate(watcher=watcher, resp=resp):
+                    print("Invalid response from watcher")
+                    return
+
+                wat = self.processWatcherResponse(watcher=watcher, icp=bytes(resp.body))
+
+                self.remove([clientDoer])
+
+                self.cues.append(dict(old=watcher, new=wat))
+
+                yield self.tock
+
+            yield self.tock
+
+
+    def processWatcherResponse(self, watcher, icp):
+        ctrlKvy = eventing.Kevery(db=self.hab.db)
+        parsing.Parser().parse(ims=bytearray(icp), kvy=ctrlKvy)
+
+        srdr = coring.Serder(raw=bytearray(icp))
+        wat = srdr.pre
+
+        habr = self.hab.db.habs.get(self.hab.name)
+        ewats = set(habr.watchers)
+
+        ewats.remove(watcher)
+        ewats.add(wat)
+
+        habr.watchers = list(ewats)
+
+        self.hab.db.habs.pin(self.hab.name, habr)
+        return wat
+
+
+    @staticmethod
+    def authenticate(watcher, resp):
+        if "Signature" not in resp.headers:
+            return False
+
+        signages = ending.designature(resp.headers["Signature"])
+
+        cigar = signages[0].markers[watcher]
+        verfer = coring.Verfer(qb64=watcher)
+        if not verfer.verify(cigar.raw, bytes(resp.body)):
+            return False
 
         return True
