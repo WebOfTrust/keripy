a6561add8b584b4d951a294a9224eb54a7906517
Kevin Griffin <griffin.kev@gmail.com>
2021-09-27 10:07:17 -0400

    adds delegation test
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- tests/app/test_delegating.py
+++ tests/app/test_delegating.py
@@ -1,23 +1,22 @@
-import json
-
 from keri.app import habbing, delegating, keeping
+from keri.core import eventing, parsing, coring
 from keri.db import basing
 
 
 def test_delegating():
-    # delegator
-    with habbing.openHab(name="del", salt=b'0123456789abcdef', transferable=True, temp=True) as delHab:
-        assert delHab.pre == "E5R24em6RjYzygDkAqM2Sr3cYkFJIObwxc7bvJ68w0rU"
-
+    # Dan is the delegator
+    # Deb is the delegatee, or the entity "requesting" delegation of an identifier
+    with habbing.openHab(name="dan", salt=b'0123456789abcdef', transferable=True, temp=True) as danHab:
         # delegatee
         ks = keeping.Keeper(name="deb", temp=True)
         ks.reopen()
         db = basing.Baser(name="deb", temp=True)
         db.reopen()
 
+        # start delegation
         delegatey = delegating.Delegatey(db=db, ks=ks)
         msg = dict(
-            delpre="E5R24em6RjYzygDkAqM2Sr3cYkFJIObwxc7bvJ68w0rU",
+            delpre=danHab.pre,
             salt="0123456789abcdef",
             transferable=True,
             icount=1,
@@ -25,13 +24,67 @@ def test_delegating():
             isith=1,
             nsith=1,
         )
+        # results in processing of delcept
         delegatey.processMessage(msg)
+        debHab = delegatey.hab
 
-        print(delegatey.posts[0])
-        print(delegatey.posts[0]["srdr"].pretty())
+        danpre = danHab.pre
+        debpre = debHab.pre
+        assert danpre == "EqueYwA9skz3SLqOYe8Lu4vzYDIJZSU984yrK3l1bEvw"
+        assert debpre == "Emyq6e6iYwcNGfHCThc4GgP4_TEfnYgCX9WhqnIhTrlE"
 
         delsrdr = delegatey.posts[0]["srdr"]
+        delsigers = delegatey.posts[0]["sigers"]
+
         assert delsrdr.ked["t"] == "dip"
-        assert delsrdr.ked["i"] == "EF9yk0lgzGXTJxDLmJitz07EUwdOrBghWUYamcQLlISw"
-        assert delsrdr.ked["di"] == "E5R24em6RjYzygDkAqM2Sr3cYkFJIObwxc7bvJ68w0rU"
-        # assert delegatey.posts[0]["srdr"].pre == []
+        assert delsrdr.ked["i"] == debpre
+        assert delsrdr.ked["di"] == danpre
+
+        assert debHab.kvy.cues[0]["kin"] == "delegatage"
+        assert debHab.kvy.cues[0]["delpre"] == danpre
+
+        # delegator
+        dankvy = eventing.Kevery(db=danHab.db, lax=False, local=False)
+        evt = eventing.messagize(serder=delsrdr, sigers=delsigers)
+
+        # process an incoming delcept
+        parsing.Parser().parse(ims=evt, kvy=dankvy)
+        srdr = dankvy.cues[0]["serder"]
+        assert dankvy.cues[0]["kin"] == "delegating"
+        assert srdr.ked["t"] == "dip"
+        assert srdr.ked["i"] == debpre
+        assert srdr.ked["di"] == danpre
+
+        # business logic outstanding, approve delegation automagically with interact
+        msg = danHab.interact(data=[
+            dict(i=srdr.pre, s=srdr.ked["s"], d=srdr.dig)
+        ])
+
+        isrdr = coring.Serder(raw=msg)
+        assert isrdr.ked["i"] == danpre
+        assert isrdr.ked["t"] == "ixn"
+        assert isrdr.ked["a"][0]["i"] == debpre
+        assert isrdr.ked["a"][0]["s"] == "0"
+        assert isrdr.ked["a"][0]["d"] == "EObmnyZtLKidoMUUdS-l90jXwiPvhYivablDGa1UbZNI"
+
+        danHab.kvy.processEscrows()
+        # after process interact and escrow, ensure we have the out of escrow event
+        assert danHab.kvy.cues[2]["kin"] == "psUnescrow"
+        assert danHab.kvy.cues[2]["serder"].ked["i"] == debpre
+        assert danHab.kvy.cues[2]["serder"].ked["di"] == danpre
+
+        # process dan's (delegator) events with kvy for deb (delegatee)
+        debkvy = eventing.Kevery(db=debHab.db, lax=False, local=False)
+        parsing.Parser().parse(ims=danHab.makeOwnEvent(sn=0), kvy=debkvy)
+        parsing.Parser().parse(ims=msg, kvy=debkvy)
+
+        debHab.kvy.processEscrows()
+        # after process interact and escrow, ensure we have the out of escrow event
+        assert debHab.kvy.cues[2]["kin"] == "psUnescrow"
+        assert debHab.kvy.cues[2]["pre"] == debpre
+
+        # finally ensure we can accept the delegation
+        debHab.delegationAccepted()
+
+        assert debHab.accepted is True
+        # happy delegation
