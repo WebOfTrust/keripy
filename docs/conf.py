03a760ebc4e4d3ae6008d9f2c80729f9ce098df0
Philip Feairheller <pfeairheller@gmail.com>
2022-02-01 12:11:03 -0500

    Publish docs to ReadTheDocs  (#138)
    This PR includes:
    
    * HTML Sphinx generated docs from docstrings
    * First pass as a docs landing page
    * Updated badges.
    
    
    Signed-off-by: Phil Feairheller <pfeairheller@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,11 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
 import keri  # noqa: E402
 
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    sphinx_rtd_theme = None
+
 project = 'keri'
 copyright = '2022, Dr. Samuel Smith and contributors'
 author = 'Dr. Samuel Smith'
@@ -55,20 +60,13 @@ napoleon_include_init_with_doc = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+if sphinx_rtd_theme:
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = "default"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# THEME
-
-# on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
