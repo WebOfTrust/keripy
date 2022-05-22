00234fb1e3a12f23ef6bf60417737ea602befd68
Kevin Griffin <griffin.kev@gmail.com>
2022-05-22 11:48:02 -0400

    reset demo ports

--- src/keri/app/cli/commands/agent/demo.py
+++ src/keri/app/cli/commands/agent/demo.py
@@ -31,18 +31,18 @@ def demo(args):
                            configDir="./scripts", insecure=True, tcp=5721, adminHttpPort=5723,
                            path=start.STATIC_DIR_PATH)
 
-    # kli agent start --config-dir ./scripts --config-file demo-witness-oobis --insecure --tcp 5821 -a 5823
-    servery2 = booting.Servery(port=5522)
+    # kli agent start --config-dir ./scripts --config-file demo-witness-oobis --insecure --tcp 5821 -a 5821
+    servery2 = booting.Servery(port=5821)
     doers += booting.setup(servery=servery2, controller="E59KmDbpjK0tRf9Rmc7OlueZVz7LB94DdD3cjQVvPcng",
                            configFile=args.configFile,
-                           configDir="./scripts", insecure=True, tcp=5520, adminHttpPort=5522,
+                           configDir="./scripts", insecure=True, tcp=5821, adminHttpPort=5821,
                            path=start.STATIC_DIR_PATH)
 
-    # kli agent start --config-dir ./scripts --config-file demo-witness-oobis --insecure --tcp 5921 -a 5923
-    servery3 = booting.Servery(port=5523)
+    # kli agent start --config-dir ./scripts --config-file demo-witness-oobis --insecure --tcp 5821 -a 5821
+    servery3 = booting.Servery(port=5821)
     doers += booting.setup(servery=servery3, controller="E59KmDbpjK0tRf9Rmc7OlueZVz7LB94DdD3cjQVvPcng",
                            configFile=args.configFile,
-                           configDir="./scripts", insecure=True, tcp=5521, adminHttpPort=5523,
+                           configDir="./scripts", insecure=True, tcp=5821, adminHttpPort=5821,
                            path=start.STATIC_DIR_PATH)
 
     return doers + [servery0, servery1, servery2, servery3]
