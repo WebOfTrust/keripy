de393898442573ef60bbfb8db814ef2fe0fb0f14
Samuel M Smith <sam@samuelsmith.org>
2022-09-03 13:31:04 -0600

    fixed a demo test

--- tests/demo/test_demo.py
+++ tests/demo/test_demo.py
@@ -22,25 +22,29 @@ def test_direct_mode_bob_eve_demo():
     """
     help.ogler.resetLevel(level=logging.DEBUG)
 
-    # set of secrets  (seeds for private keys)
-    bobSecrets = [
-                'ArwXoACJgOleVZ2PY7kXn7rA0II0mHYDhc6WrBH8fDAc',
-                'A6zz7M08-HQSFq92sJ8KJOT2cZ47x7pXFQLPB0pckB3Q',
-                'AcwFTk-wgk3ZT2buPRIbK-zxgPx-TKbaegQvPEivN90Y',
-                'Alntkt3u6dDgiQxTATr01dy8M72uuaZEf9eTdM-70Gk8',
-                'A1-QxDkso9-MR1A8rZz_Naw6fgaAtayda8hrbkRVVu1E',
-                'AKuYMe09COczwf2nIoD5AE119n7GLFOVFlNLxZcKuswc',
-                'AxFfJTcSuEE11FINfXMqWttkZGnUZ8KaREhrnyAXTsjw',
-                'ALq-w1UKkdrppwZzGTtz4PWYEeWm0-sDHzOv5sq96xJY'
-                ]
+    ## set of secrets  (seeds for private keys)
+    #bobSecrets = [
+                #'ArwXoACJgOleVZ2PY7kXn7rA0II0mHYDhc6WrBH8fDAc',
+                #'A6zz7M08-HQSFq92sJ8KJOT2cZ47x7pXFQLPB0pckB3Q',
+                #'AcwFTk-wgk3ZT2buPRIbK-zxgPx-TKbaegQvPEivN90Y',
+                #'Alntkt3u6dDgiQxTATr01dy8M72uuaZEf9eTdM-70Gk8',
+                #'A1-QxDkso9-MR1A8rZz_Naw6fgaAtayda8hrbkRVVu1E',
+                #'AKuYMe09COczwf2nIoD5AE119n7GLFOVFlNLxZcKuswc',
+                #'AxFfJTcSuEE11FINfXMqWttkZGnUZ8KaREhrnyAXTsjw',
+                #'ALq-w1UKkdrppwZzGTtz4PWYEeWm0-sDHzOv5sq96xJY'
+                #]
 
-    bobSecrecies = []
-    for secret in bobSecrets:  # convert secrets to secrecies
-        bobSecrecies.append([secret])
+    #bobSecrecies = []
+    #for secret in bobSecrets:  # convert secrets to secrecies
+        #bobSecrecies.append([secret])
 
-    #  create bob signers
-    bobSigners = [coring.Signer(qb64=secret) for secret in bobSecrets]
-    assert [signer.qb64 for signer in bobSigners] == bobSecrets
+    ##  create bob signers
+    #bobSigners = [coring.Signer(qb64=secret) for secret in bobSecrets]
+    #assert [signer.qb64 for signer in bobSigners] == bobSecrets
+
+    #  create bob signers and secrecies
+    bobSigners = coring.Salter(raw=b"raw salt for bob").signers(count=8, path="bob", temp=True)
+    bobSecrecies = [[signer.qb64] for signer in bobSigners]
 
     # bob inception transferable (nxt digest not empty)
     bobSerder = eventing.incept(keys=[bobSigners[0].verfer.qb64],
@@ -48,24 +52,28 @@ def test_direct_mode_bob_eve_demo():
                                 code=coring.MtrDex.Blake3_256)
 
     bob = bobSerder.ked["i"]
-    assert bob == 'EdwS_D6wppLqfIp5LSgly8GTScg5OWBaa7thzEnBqHvw'
+    assert bob == 'EL5WSz5d7WvE53oCsIdWnJc50yocmfpvMfS2rf0i3ZGq'  # 'EdwS_D6wppLqfIp5LSgly8GTScg5OWBaa7thzEnBqHvw'
 
-    eveSecrets = ['AgjD4nRlycmM5cPcAkfOATAp8wVldRsnc9f1tiwctXlw',
-                  'AKUotEE0eAheKdDJh9QvNmSEmO_bjIav8V_GmctGpuCQ',
-                  'AK-nVhMMJciMPvmF5VZE_9H-nhrgng9aJWf7_UHPtRNM',
-                  'AT2cx-P5YUjIw_SLCHQ0pqoBWGk9s4N1brD-4pD_ANbs',
-                  'Ap5waegfnuP6ezC18w7jQiPyQwYYsp9Yv9rYMlKAYL8k',
-                  'Aqlc_FWWrxpxCo7R12uIz_Y2pHUH2prHx1kjghPa8jT8',
-                  'AagumsL8FeGES7tYcnr_5oN6qcwJzZfLKxoniKUpG4qc',
-                  'ADW3o9m3udwEf0aoOdZLLJdf1aylokP0lwwI_M2J9h0s']
+    #eveSecrets = ['AgjD4nRlycmM5cPcAkfOATAp8wVldRsnc9f1tiwctXlw',
+                  #'AKUotEE0eAheKdDJh9QvNmSEmO_bjIav8V_GmctGpuCQ',
+                  #'AK-nVhMMJciMPvmF5VZE_9H-nhrgng9aJWf7_UHPtRNM',
+                  #'AT2cx-P5YUjIw_SLCHQ0pqoBWGk9s4N1brD-4pD_ANbs',
+                  #'Ap5waegfnuP6ezC18w7jQiPyQwYYsp9Yv9rYMlKAYL8k',
+                  #'Aqlc_FWWrxpxCo7R12uIz_Y2pHUH2prHx1kjghPa8jT8',
+                  #'AagumsL8FeGES7tYcnr_5oN6qcwJzZfLKxoniKUpG4qc',
+                  #'ADW3o9m3udwEf0aoOdZLLJdf1aylokP0lwwI_M2J9h0s']
 
-    eveSecrecies = []
-    for secret in eveSecrets:  # convert secrets to secrecies
-        eveSecrecies.append([secret])
+    #eveSecrecies = []
+    #for secret in eveSecrets:  # convert secrets to secrecies
+        #eveSecrecies.append([secret])
 
-    #  create eve signers
-    eveSigners = [coring.Signer(qb64=secret) for secret in eveSecrets]
-    assert [signer.qb64 for signer in eveSigners] == eveSecrets
+    ##  create eve signers
+    #eveSigners = [coring.Signer(qb64=secret) for secret in eveSecrets]
+    #assert [signer.qb64 for signer in eveSigners] == eveSecrets
+
+    #  create bob signers and secrecies
+    eveSigners = coring.Salter(raw=b"raw salt for eve").signers(count=8, path="eve", temp=True)
+    eveSecrecies = [[signer.qb64] for signer in eveSigners]
 
     # eve inception transferable (nxt digest not empty)
     eveSerder = eventing.incept(keys=[eveSigners[0].verfer.qb64],
@@ -73,11 +81,10 @@ def test_direct_mode_bob_eve_demo():
                                 code=coring.MtrDex.Blake3_256)
 
     eve = eveSerder.ked["i"]
-    assert eve == 'E0VtKUgXnnXq9EtfgKAd_l5lhyhx_Rlf0Uj1XejaNNoo'
+    assert eve == 'ECqgMm2DLrJlVA4Gyjz6Djct3F4Z86ztyFy6-y8Y_yu_'  # 'E0VtKUgXnnXq9EtfgKAd_l5lhyhx_Rlf0Uj1XejaNNoo'
 
-    with habbing.openHby(name="eve", base="test") as eveHby, \
-         habbing.openHby(name="bob", base="test") as bobHby:
-
+    with (habbing.openHby(name="eve", base="test") as eveHby,
+          habbing.openHby(name="bob", base="test") as bobHby):
 
         limit = 1.0
         tock = 0.03125
