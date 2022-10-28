743530ec4ff555f091591228ee235c612eb902e9
Philip Feairheller <pfeairheller@gmail.com>
2022-10-27 19:54:30 -0700

    fix start-witness.sh by adding correct config file and path in incept command (#352)

--- scripts/demo/basic/start-witness.sh
+++ scripts/demo/basic/start-witness.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kli init --name witness --nopasscode
-kli incept --name witness --alias=wil --file ${KERI_DEMO_SCRIPT_DIR}/data/wil-witness-sample.json
-kli witness start --name witness --alias=wil
+kli init --name witness --nopasscode --config-dir ${KERI_SCRIPT_DIR} --config-file witness
+kli incept --name witness --alias witness --config ${KERI_SCRIPT_DIR} --file ${KERI_DEMO_SCRIPT_DIR}/data/wil-witness-sample.json
+kli witness start --name witness --alias witness
