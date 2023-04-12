dc2095dd8062813a0f20409071d8cfb085f309f2
Philip Feairheller <pfeairheller@gmail.com>
2023-04-12 16:43:27 -0700

    Query rewrite and bug fix (#478)
    * Rewrite Query as a real Doer, without doing.doify.
    
    * Fix OOBIEnd to not reply to an OOBI request for an AID that doesn't have a threshold satisfying number of witness receipts.
    
    * Removing dependency on codecov PyPi package that was unceremoniously removed from PyPi today.
    
    * Attempt to reestablish codecov upload using github action.
    
    
    
    ---------
    
    Signed-off-by: pfeairheller <pfeairheller@gmail.com>

--- tests/app/test_querying.py
+++ tests/app/test_querying.py
@@ -6,14 +6,13 @@ keri.app.querying module
 from hio.base import doing
 
 from keri.app import habbing
-from keri.app.querying import QueryDoer
+from keri.app.querying import QueryDoer, KeyStateNoticer, LogQuerier
 from keri.core import parsing
 
 
 def test_querying():
     with habbing.openHby() as hby, \
             habbing.openHby() as hby1:
-        print()
         inqHab = hby.makeHab(name="inquisitor")
         subHab = hby1.makeHab(name="subject")
         qdoer = QueryDoer(hby=hby, hab=inqHab, kvy=hby.kvy, pre=subHab.pre)
@@ -29,15 +28,19 @@ def test_querying():
 
         # doist.do(doers=doers)
         deeds = doist.enter(doers=[qdoer])
-        doist.recur(deeds=deeds)
 
-        assert len(qdoer.witq.msgs) == 1
-        msg = qdoer.witq.msgs.popleft()
+        assert len(qdoer.doers) == 1
+        ksnDoer = qdoer.doers[0]
+        assert isinstance(ksnDoer, KeyStateNoticer)
+        assert len(ksnDoer.witq.msgs) == 1
+        msg = ksnDoer.witq.msgs.popleft()
         assert msg["src"] == inqHab.pre
         assert msg["pre"] == subHab.pre
         assert msg["r"] == "ksn"
         assert msg["q"] == {'s': 0}
         assert msg["wits"] is None
+
+        doist.recur(deeds=deeds)
 
         # Cue up a saved key state equal to the one we have
         hby.kvy.cues.clear()
@@ -48,7 +51,7 @@ def test_querying():
         doist.recur(deeds=deeds)
 
         # We already have up to date key state so loaded will be true
-        assert qdoer.loaded is True
+        assert qdoer.done is True
         assert len(hby.kvy.cues) == 0
 
         # create a new query doer
@@ -58,25 +61,26 @@ def test_querying():
         doist = doing.Doist(limit=limit, tock=tock, real=True)
 
         # rotate AID and submit as a new keyStateSave
-        subHab.rotate()
+        rot = subHab.rotate()
         ksn = subHab.kever.state()
         cue = dict(kin="keyStateSaved", serder=ksn)
         hby.kvy.cues.append(cue)
-
         deeds = doist.enter(doers=[qdoer])
         doist.recur(deeds=deeds)
 
         # We are behind in key state, so we aren't done and have queried for the logs
-        assert qdoer.loaded is False
-        assert len(qdoer.witq.msgs) == 2
-        msg = qdoer.witq.msgs[0]
-        assert msg["src"] == inqHab.pre
-        assert msg["pre"] == subHab.pre
-        assert msg["r"] == "logs"
-        assert msg["q"] == {'s': 0}
-        assert msg["wits"] is None
+        assert qdoer.done is False
+        assert len(qdoer.doers) == 1
+        ksnDoer = qdoer.doers[0]
+        assert isinstance(ksnDoer, KeyStateNoticer)
+        assert len(ksnDoer.witq.msgs) == 1
 
-        icp = subHab.makeOwnEvent(sn=1)
-        parsing.Parser().parseOne(ims=bytearray(icp), kvy=inqHab.kvy)
+        assert len(ksnDoer.doers) == 1
+        logDoer = ksnDoer.doers[0]
+        assert isinstance(logDoer, LogQuerier)
+        assert len(hby.kvy.cues) == 0
+
+        parsing.Parser().parseOne(ims=bytearray(rot), kvy=inqHab.kvy)
         doist.recur(deeds=deeds)
-        assert qdoer.loaded is True
+
+        assert qdoer.done is True
