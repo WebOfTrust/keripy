d558f14490fe8339e8feb1d92e3d99995b3b144e
Nuttawut Kongsuwan <58167639+nkongsuwan@users.noreply.github.com>
2023-09-30 21:19:27 +0700

    Correct the KLI description for TOAD (#580)
    TOAD is threshold of accountable duplicity, not threshold of acceptable duplicity.

--- src/keri/app/cli/common/incepting.py
+++ src/keri/app/cli/common/incepting.py
@@ -15,7 +15,7 @@ def addInceptingArgs(parser):
     parser.add_argument('--wits',         '-w', default=[], required=False, action="append", metavar="<prefix>",
                         help='New set of witnesses, replaces all existing witnesses.  Can appear multiple times')
     parser.add_argument('--toad',         '-t', default=None, required=False, type=int,
-                        help='int or str hex of witness threshold (threshold of acceptable duplicity)',)
+                        help='int or str hex of witness threshold (threshold of accountable duplicity)',)
     parser.add_argument('--icount',       '-ic', default=None, required=False,
                         help='incepting key count for number of keys used for inception')
     parser.add_argument('--isith',        '-s', default=None, required=False,
