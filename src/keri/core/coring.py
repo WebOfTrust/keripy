e494e6ee65ad93a7f16bc326b9f46b219c4b19ab
Samuel M Smith <sam@samuelsmith.org>
2024-02-23 10:08:32 -0700

    Added placeholder Streamer class

--- src/keri/core/coring.py
+++ src/keri/core/coring.py
@@ -1861,6 +1861,82 @@ class Dater(Matter):
         return helping.fromIso8601(self.dts)
 
 
+class Streamer:
+    """
+    Streamer is CESR sniffable stream class
+
+
+    Has the following public properties:
+
+    Properties:
+
+
+    Methods:
+
+
+    Hidden:
+
+
+
+    """
+
+    def __init__(self, stream):
+        """Initialize instance
+
+
+        Parameters:
+            stream (bytes | bytearray): sniffable CESR stream
+
+
+        """
+        self._stream = bytes(stream)
+
+
+    @property
+    def stream(self):
+        """stream property getter
+        """
+        return self._stream
+
+    @property
+    def text(self):
+        """expanded stream as qb64 text
+        Returns:
+           stream (bytes): expanded text qb64 version of stream
+
+        """
+        return self._stream
+
+    @property
+    def binary(self):
+        """compacted stream as qb2 binary
+        Returns:
+           stream (bytes): compacted binary qb2 version of stream
+
+        """
+        return self._stream
+
+    @property
+    def texter(self):
+        """expanded stream as Texter instance
+        Returns:
+           texter (Texter): Texter primitive of stream suitable wrapping
+
+        """
+        return self._stream
+
+    @property
+    def bexter(self):
+        """expanded stream as Bexter instance
+        Returns:
+           bexter (Bexter): Bexter primitive as stream suitable wrapping
+
+        """
+        return self._stream
+
+
+
+
 class Texter(Matter):
     """
     Texter is subclass of Matter, cryptographic material, for variable length
