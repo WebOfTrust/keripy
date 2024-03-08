5bdaaec7a75b5fcd0ad9d76aaecf98555884f492
Philip Feairheller <pfeairheller@gmail.com>
2024-03-08 09:09:47 -0800

    Includes 2 fixes (#705)
    - Fix to saving SignifyGroupHabs to enable reloading
    - Fix to credential export to support exporting all credentials.
    
    Signed-off-by: pfeairheller <pfeairheller@gmail.com>

--- src/keri/app/cli/commands/migrate.py
+++ src/keri/app/cli/commands/migrate.py
@@ -9,8 +9,11 @@ import argparse
 from hio import help
 from hio.base import doing
 
+from keri import kering
 from keri.app.cli.common import existing
-from keri.db import koming
+from keri.core import coring, serdering
+from keri.db import koming, subing, dbing
+from keri.db.basing import KeyStateRecord, StateEERecord
 from keri.kering import ConfigurationError, Version
 from keri.vdr import viring
 
@@ -24,9 +27,22 @@ parser.add_argument('--base', '-b', help='additional optional prefix to file loc
                     required=False, default="")
 parser.add_argument('--passcode', '-p', help='22 character encryption passcode for keystore (is not saved)',
                     dest="bran", default=None)  # passcode => bran
+parser.add_argument('--force', action="store_true", required=False,
+                    help='True means perform migration without prompting the user')
 
 
 def handler(args):
+    if not args.force:
+        print()
+        print("This command will migrate your datastore to the next version of KERIpy and is not reversible.")
+        print("After this command, you will not be able to access your data store with this version.")
+        print()
+        yn = input("Are you sure you want to continue? [y|N]: ")
+
+        if yn not in ("y", "Y"):
+            print("...exiting")
+            return []
+
     kwa = dict(args=args)
     return [doing.doify(migrate, **kwa)]
 
@@ -42,15 +58,47 @@ def migrate(tymth, tock=0.0, **opts):
     bran = args.bran
 
     try:
-        with existing.existingHby(name=name, base=base, bran=bran) as hby:
-            rgy = viring.Reger(name=name, base=base, db=hby.db, temp=hby.temp,
-                               reopen=True)
-
-            states = koming.Komer(db=rgy,
+        with dbing.openLMDB(name=name, base=base, bran=bran, temp=False) as db:
+            print(db.path)
+            states = koming.Komer(db=db,
                                   schema=dict,
                                   subkey='stts.')
+            nstates = koming.Komer(db=db,
+                                   schema=KeyStateRecord,
+                                   subkey='stts.')
 
-            for _, sad in states.getItemIter():
+            for keys, sad in states.getItemIter():
+                ksr = KeyStateRecord(
+                    vn=Version,  # version number as list [major, minor]
+                    i=sad['i'],  # qb64 prefix
+                    s=sad['s'],  # lowercase hex string no leading zeros
+                    p=sad['p'],
+                    d=sad['d'],
+                    f=sad['f'],  # lowercase hex string no leading zeros
+                    dt=sad['dt'],
+                    et=sad['et'],
+                    kt=sad['kt'],
+                    k=sad['k'],
+                    nt=sad['nt'],
+                    n=sad['n'],
+                    bt=sad['bt'],
+                    b=sad['b'],
+                    c=sad['c'],
+                    ee=StateEERecord._fromdict(sad['ee']),  # latest est event dict
+                    di=sad['di'] if sad['di'] else None
+                )
+
+                nstates.pin(keys=keys, val=ksr)
+
+        with existing.existingHby(name=name, base=base, bran=bran) as hby:
+            rgy = viring.Reger(name=name, base=base, db=hby.db, temp=False,
+                               reopen=True)
+
+            rstates = koming.Komer(db=rgy,
+                                   schema=dict,
+                                   subkey='stts.')
+
+            for _, sad in rstates.getItemIter():
                 rsr = viring.RegStateRecord(
                     vn=list(Version),  # version number as list [major, minor]
                     i=sad['i'],  # qb64 registry SAID
@@ -66,6 +114,40 @@ def migrate(tymth, tock=0.0, **opts):
                 # ksr = stateFromKever(kever)
                 rgy.states.pin(sad['i'], val=rsr)
 
+            for (said,), _ in rgy.creds.getItemIter():
+                snkey = dbing.snKey(said, 0)
+                dig = rgy.getTel(key=snkey)
+
+                prefixer = coring.Prefixer(qb64=said)
+                seqner = coring.Seqner(sn=0)
+                saider = coring.Saider(qb64b=bytes(dig))
+                rgy.cancs.pin(keys=said, val=[prefixer, seqner, saider])
+
+            migrateKeys(hby.db)
+
     except ConfigurationError:
         print(f"identifier prefix for {name} does not exist, incept must be run first", )
         return -1
+
+
+def migrateKeys(db):
+    # public keys mapped to the AID and event seq no they appeared in
+    pubs = subing.CatCesrIoSetSuber(db=db, subkey="pubs.",
+                                    klas=(coring.Prefixer, coring.Seqner))
+
+    # next key digests mapped to the AID and event seq no they appeared in
+    digs = subing.CatCesrIoSetSuber(db=db, subkey="digs.",
+                                    klas=(coring.Prefixer, coring.Seqner))
+
+    for pre, fn, dig in db.getFelItemAllPreIter(key=b''):
+        dgkey = dbing.dgKey(pre, dig)  # get message
+        if not (raw := db.getEvt(key=dgkey)):
+            raise kering.MissingEntryError("Missing event for dig={}.".format(dig))
+        serder = serdering.SerderKERI(raw=bytes(raw))
+        val = (coring.Prefixer(qb64b=serder.preb), coring.Seqner(sn=serder.sn))
+        verfers = serder.verfers or []
+        for verfer in verfers:
+            pubs.add(keys=(verfer.qb64,), val=val)
+        ndigers = serder.ndigers or []
+        for diger in ndigers:
+            digs.add(keys=(diger.qb64,), val=val)
