1fcdc18a928283564c0f4cdd18a50d42a6015876
Kent Bull <kent@kentbull.com>
2025-04-23 18:48:12 -0600

    added "rp" message fix, logging fixes, makefile updates, and bumped version

--- tests/core/test_serdering.py
+++ tests/core/test_serdering.py
@@ -3315,6 +3315,17 @@ def test_cesr_native_dumps_hby():
 
     """End Test"""
 
+def test_serderkeri_exn_old():
+    """Test SerderKERI exn msg"""
+    serder = SerderKERI(raw=b'{"v":"KERI10JSON000088_","t":"exn",'
+                   b'"d":"EMuAoRSE4zREKKYyvuNeYCDM9_MwPQIh1WL0'
+                   b'cFC4e-bU","i":"","p":"","dt":"","r":"","q":{},"a":[],"e":{}}', ilk=kering.Ilks.exn)
+
+    assert serder.verify()  # because pre is empty
+    assert serder.ilk == kering.Ilks.exn
+    assert serder.pre == ''
+    assert serder.prior == ''
+
 if __name__ == "__main__":
     test_fielddom()
     test_spans()
@@ -3338,4 +3349,5 @@ if __name__ == "__main__":
     test_serdery()
     test_cesr_native_dumps()
     test_cesr_native_dumps_hby()
+    test_serderkeri_exn_old()
 
