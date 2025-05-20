870b103044624ddd2c4d96e47f8d7e749ff6b2d1
Kevin Griffin <griffin.kev@gmail.com>
2025-05-20 10:23:16 -0400

    fixes imports (#1004)
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- src/keri/app/cli/commands/local/watch.py
+++ src/keri/app/cli/commands/local/watch.py
@@ -10,6 +10,7 @@ import time
 
 from keri import help
 from hio.base import doing
+from hio.help import decking
 from keri.app import agenting, indirecting, habbing, forwarding
 from keri.app.cli.common import existing, terming
 from keri.app.habbing import GroupHab
@@ -44,7 +45,7 @@ class WatchDoer(doing.DoDoer):
         doers = []
         self.hby = existing.setupHby(name=name, base=base, bran=bran)
         self.hbyDoer = habbing.HaberyDoer(habery=self.hby)  # setup doer
-        self.cues = help.decking.Deck()
+        self.cues = decking.Deck()
 
         self.mbd = indirecting.MailboxDirector(hby=self.hby, topics=["/replay", "/receipt", "/reply"])
         self.postman = forwarding.Poster(hby=self.hby)
