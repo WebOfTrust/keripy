82241dad82e5772db9c1208cb89c67ec7c9fcc41
lenkan <dlenksjo@gmail.com>
2025-06-05 15:13:12 +0200

    remove use of backround script functions

--- scripts/demo/basic/multisig-delegator.sh
+++ scripts/demo/basic/multisig-delegator.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 set -e
 
-. "$(dirname "$0")/../demo-scripts.sh"
 . "$(dirname "$0")/script-utils.sh"
 
 delegator_1=$(random_name delegator_1)
 delegator_2=$(random_name delegator_2)
 delegate=$(random_name delegate)
 
-background_start kli init --name "$delegator_1" --nopasscode
-background_start kli init --name "$delegator_2" --nopasscode
-background_start kli init --name "$delegate" --nopasscode
-background_wait
+kli init --name "$delegator_1" --nopasscode
+kli init --name "$delegator_2" --nopasscode
+kli init --name "$delegate" --nopasscode
 
 delegator_witness_aid="BLskRTInXnMxWaGqcpSyMgo0nYbalW99cGZESrz3zapM"
 delegator_witness_url="http://127.0.0.1:5643/oobi/$delegator_witness_aid/controller"
 delegate_witness_aid="BBilc4-L3tFUnfM_wJr4S4OJanAv_VmF_dJNN6vkf2Ha"
 delegate_witness_url="http://127.0.0.1:5642/oobi/$delegate_witness_aid/controller"
 
-background_start kli oobi resolve --name "$delegator_1" --oobi "$delegator_witness_url"
-background_start kli oobi resolve --name "$delegator_2" --oobi "$delegator_witness_url"
-background_start kli oobi resolve --name "$delegate" --oobi "$delegate_witness_url"
-background_wait
+kli oobi resolve --name "$delegator_1" --oobi "$delegator_witness_url"
+kli oobi resolve --name "$delegator_2" --oobi "$delegator_witness_url"
+kli oobi resolve --name "$delegate" --oobi "$delegate_witness_url"
 
 kli incept --name "$delegator_1" --alias member --icount 1 --ncount 1 --isith 1 --nsith 1 --transferable --toad 1 --wit "$delegator_witness_aid"
 kli incept --name "$delegator_2" --alias member --icount 1 --ncount 1 --isith 1 --nsith 1 --transferable --toad 1 --wit "$delegator_witness_aid"
@@ -33,9 +30,8 @@ delegator_1_oobi=$(kli oobi generate --name "$delegator_1" --alias member --role
 delegator_2_aid=$(kli aid --name "$delegator_2" --alias member)
 delegator_2_oobi=$(kli oobi generate --name "$delegator_2" --alias member --role witness | tail -n 1)
 
-background_start kli oobi resolve --name "$delegator_1" --oobi "$delegator_2_oobi"
-background_start kli oobi resolve --name "$delegator_2" --oobi "$delegator_1_oobi"
-background_wait
+kli oobi resolve --name "$delegator_1" --oobi "$delegator_2_oobi"
+kli oobi resolve --name "$delegator_2" --oobi "$delegator_1_oobi"
 
 delegator_json=$(mktemp)
 cat << EOF > "$delegator_json"
@@ -49,9 +45,10 @@ cat << EOF > "$delegator_json"
 }
 EOF
 
-background_start kli multisig incept --name "$delegator_1" --alias member --group delegator --file "$delegator_json"
-background_start kli multisig incept --name "$delegator_2" --alias member --group delegator --file "$delegator_json"
-background_wait
+kli multisig incept --name "$delegator_1" --alias member --group delegator --file "$delegator_json" &
+pid=$!
+kli multisig incept --name "$delegator_2" --alias member --group delegator --file "$delegator_json"
+wait $pid
 
 # Create proxy and resolve OOBIs
 kli incept --name "$delegate" --alias proxy --icount 1 --ncount 1 --isith 1 --nsith 1 --transferable --toad 1 --wit "$delegate_witness_aid"
@@ -60,10 +57,9 @@ proxy_oobi=$(kli oobi generate --name "$delegate" --alias proxy --role witness |
 delegator_oobi=$(kli oobi generate --name "$delegator_1" --alias delegator --role witness | tail -n 1)
 delegator_aid=$(kli aid --name "$delegator_1" --alias delegator)
 
-background_start kli oobi resolve --name "$delegate" --oobi-alias delegator --oobi "${delegator_oobi}"
-background_start kli oobi resolve --name "$delegator_1" --oobi-alias proxy --oobi "${proxy_oobi}"
-background_start kli oobi resolve --name "$delegator_2" --oobi-alias proxy --oobi "${proxy_oobi}"
-background_wait
+kli oobi resolve --name "$delegate" --oobi-alias delegator --oobi "${delegator_oobi}"
+kli oobi resolve --name "$delegator_1" --oobi-alias proxy --oobi "${proxy_oobi}"
+kli oobi resolve --name "$delegator_2" --oobi-alias proxy --oobi "${proxy_oobi}"
 
 delegate_json=$(mktemp)
 cat << EOF > "$delegate_json"
@@ -80,9 +76,12 @@ cat << EOF > "$delegate_json"
 EOF
 
 # Create delegated identifier
-background_start kli incept --name "$delegate" --alias delegate --proxy proxy --file "$delegate_json"
-background_start kli delegate confirm --name "$delegator_1" --alias delegator --interact -Y
-background_start kli delegate confirm --name "$delegator_2" --alias delegator --interact -Y
-background_wait
+kli incept --name "$delegate" --alias delegate --proxy proxy --file "$delegate_json" &
+PID_LIST="$!"
+kli delegate confirm --name "$delegator_1" --alias delegator --interact -Y &
+PID_LIST+=" $!"
+kli delegate confirm --name "$delegator_2" --alias delegator --interact -Y &
+PID_LIST+=" $!"
+wait $PID_LIST
 
 kli status --name "$delegate" --alias delegate
