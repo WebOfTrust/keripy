3c252b7c0fed7b1be992d5ac9b9e7b03012059f9
Samuel M Smith <sam@samuelsmith.org>
2025-06-10 15:03:24 -0600

    init done = False

--- src/keri/core/parsing.py
+++ src/keri/core/parsing.py
@@ -790,7 +790,6 @@ class Parser:
             raise  # no enclosing message group so can't preflush, must flush stream
 
         finally:  # restore version at top level
-            done = True
             while stack:  # when tail end of group has validation error
                 svrsn, _ = stack.pop()
                 if svrsn:
