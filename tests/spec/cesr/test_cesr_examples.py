444bbbee2828a3be96c43f3df87b81e28b042e2a
Samuel M Smith <sam@samuelsmith.org>
2025-08-09 16:28:35 -0600

    finished CESR spec tests

--- tests/spec/cesr/test_cesr_examples.py
+++ tests/spec/cesr/test_cesr_examples.py
@@ -5,6 +5,7 @@ tests.spec.cesr.test_cesr_example module
 """
 from base64 import urlsafe_b64encode as encodeB64
 from base64 import urlsafe_b64decode as decodeB64
+import json
 
 import pytest
 
@@ -12,9 +13,10 @@ from ordered_set import OrderedSet as oset
 
 from keri import Vrsn_2_0, Kinds, Protocols, Ilks, TraitDex
 from keri.core import (MtrDex, Salter, Signer, Diger, Noncer, Number, Structor,
-                       SealEvent, SealSource, Counter, Codens, Seqner)
+                       SealEvent, SealSource, Counter, Codens, Seqner, Pather)
 from keri.core import (incept, interact, rotate, delcept, deltate, receipt,
                        query, reply, prod, bare, exchept, exchange)
+from keri.vc import acdcmap
 
 def test_cesr_examples():
     """ Working examples for CESR specification """
@@ -66,9 +68,262 @@ def test_cesr_examples():
           b's6069pqV8rdQymrJTdcmJAEYJDJXuHUc6sjgdb0_VlPYIPtVZ9ypbRhkkuXJOykL')
 
     # Said computation
+    field0 = "field_0_01234567"
+    assert len(field0) == 16
+    field1 = "field_1_ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    assert len(field1) == 44
+    field2 = "field_2_98765432"
+    assert len(field2) == 16
+
+    dummy = "#" * 44
+    assert len(dummy) == 44
+
+    ser = field0 + field1 + field2
+    assert len(ser) == 16 + 44 + 16 == 76
+    assert ser == 'field_0_01234567field_1_ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789field_2_98765432'
+
+    dser = field0 + dummy + field2
+    assert len(dser) == len(ser) == 76
+    assert dser == 'field_0_01234567############################################field_2_98765432'
+
+    dig = Diger(ser=dser.encode()).qb64
+    assert dig == 'ENI2bDYghiu1KYYkFrPofH8tJ5tNiNt8WrTIc4s_5IIH'
+
+    cser = field0 + dig + field2
+    assert len(cser) == len(dser) == 76
+    assert cser == 'field_0_01234567ENI2bDYghiu1KYYkFrPofH8tJ5tNiNt8WrTIc4s_5IIHfield_2_98765432'
+
+
+    # SAID computation for dict
+    d = \
+    {
+        "said": "",
+        "first": "Sue",
+        "last": "Smith",
+        "role": "Founder"
+    }
+
+    dummy = '#' * 44
+
+    dd = dict(said=dummy, first="Sue", last="Smith", role="Founder")
+    assert dd == \
+    {
+        "said": "############################################",
+        "first": "Sue",
+        "last": "Smith",
+        "role": "Founder"
+    }
+
+    raw = json.dumps(dd, separators=(",", ":"), ensure_ascii=False).encode()
+    assert raw == b'{"said":"############################################","first":"Sue","last":"Smith","role":"Founder"}'
+
+    said = Diger(ser=raw).qb64
+    assert said == 'EJymtAC4piy_HkHWRs4JSRv0sb53MZJr8BQ4SMixXIVJ'
+
+    sd = dict(said=said, first="Sue", last="Smith", role="Founder")
+    assert sd == \
+    {
+        'said': 'EJymtAC4piy_HkHWRs4JSRv0sb53MZJr8BQ4SMixXIVJ',
+        'first': 'Sue',
+        'last': 'Smith',
+        'role': 'Founder'
+    }
+
+    # JSON Schema Example SAID
+    dschema =  \
+    {
+        "$id": "############################################",
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "type": "object",
+        "properties": {
+            "full_name": {
+                "type": "string"
+            }
+        }
+    }
+
+    assert dschema == \
+    {
+        '$id': '############################################',
+        '$schema': 'http://json-schema.org/draft-07/schema#',
+        'type': 'object',
+        'properties':
+        {
+            'full_name': {'type': 'string'}
+        }
+    }
+
+    raw = json.dumps(dschema, separators=(",", ":"), ensure_ascii=False).encode()
+    assert raw == (b'{"$id":"############################################","$schema":"http://json'
+                    b'-schema.org/draft-07/schema#","type":"object","properties":{"full_name":{"ty'
+                    b'pe":"string"}}}')
+
+    said = Diger(ser=raw).qb64
+    assert said == 'EGU_SHY-8ywNBJOqPKHr4sXV9tOtOwpYzYOM63_zUCDW'
+
+    dschema["$id"] = said
+
+    assert dschema == \
+    {
+        "$id": "EGU_SHY-8ywNBJOqPKHr4sXV9tOtOwpYzYOM63_zUCDW",
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "type": "object",
+        "properties":
+        {
+            "full_name": {"type": "string"}
+        }
+    }
+
+    """Done Test"""
+
+    # Sad path
+    ean = 'EPR7FWsN3tOM8PqfMap2FRfF4MFQ4v3ZXjBUcMVtvhmB'
+    amy = "ECmiMVHTfZIjhA_rovnfx73T3G_FJzIQtzDn1meBVLAz"
+    bob = "ECWJZFBtllh99fESUOrBvT3EtBujWtDKCmyzDAXWhYmf"
+    cal = "ECsGDKWAYtHBCkiDrzajkxs3Iw2g-dls3bLUsRP4yVdT"
+    deb = "EEDGM_DvZ9qFEAPf_FX08J3HX49ycrVvYVXe9isaP5SW"
+    schema = 'EGU_SHY-8ywNBJOqPKHr4sXV9tOtOwpYzYOM63_zUCDW'
+
+    paths = \
+    [
+      {
+        "ref0": {
+          "name": "Amy",
+          "i": amy
+        }
+      },
+      {
+        "ref1": {
+          "name": "Bob",
+          "i": bob
+        }
+      }
+    ]
+
+    attribute = \
+    {
+        "d": "",
+        "i": cal,
+        "dt": "2025-06-09T17:35:54.169967+00:00",
+        "personal":
+        {
+            "name": "John Doe",
+            "home": "Atlanta",
+        },
+        "p": paths,
+    }
+
+
+
+    serder = acdcmap(issuer=deb, schema=schema, attribute=attribute, issuee=cal, compactify=True)
+    assert serder.sad == \
+    {
+        'v': 'ACDCCAACAAJSONAADx.',
+        't': 'acm',
+        'd': 'EO3117lnAbjDt66qe2PtgHooXKAYQT_C6SIbESMcJ5lN',
+        'i': 'EEDGM_DvZ9qFEAPf_FX08J3HX49ycrVvYVXe9isaP5SW',
+        's': 'EGU_SHY-8ywNBJOqPKHr4sXV9tOtOwpYzYOM63_zUCDW',
+        'a': 'ED1wMKzV72L7YI1yJ3NXlClPUgvEerw4jRocOYxaZGtH'
+    }
+
+    attribute = \
+    {
+        "d": 'ED1wMKzV72L7YI1yJ3NXlClPUgvEerw4jRocOYxaZGtH',
+        "i": cal,
+        "dt": "2025-06-09T17:35:54.169967+00:00",
+        "personal":
+        {
+            "name": "John Doe",
+            "home": "Atlanta",
+        },
+        "p": paths,
+    }
+
+    serder = acdcmap(issuer=deb, schema=schema, attribute=attribute, issuee=cal)
+    assert serder.sad == \
+    {
+        "v": "ACDCCAACAAJSONAAIe.",
+        "t": "acm",
+        "d": "EO3117lnAbjDt66qe2PtgHooXKAYQT_C6SIbESMcJ5lN",
+        "i": "EEDGM_DvZ9qFEAPf_FX08J3HX49ycrVvYVXe9isaP5SW",
+        "s": "EGU_SHY-8ywNBJOqPKHr4sXV9tOtOwpYzYOM63_zUCDW",
+        "a":
+        {
+            "d": "ED1wMKzV72L7YI1yJ3NXlClPUgvEerw4jRocOYxaZGtH",
+            "i": "ECsGDKWAYtHBCkiDrzajkxs3Iw2g-dls3bLUsRP4yVdT",
+            "dt": "2025-06-09T17:35:54.169967+00:00",
+            "personal":
+            {
+                "name": "John Doe",
+                "home": "Atlanta"
+            },
+            "p":
+            [
+                {
+                    "ref0":
+                    {
+                        "name": "Amy",
+                        "i": "ECmiMVHTfZIjhA_rovnfx73T3G_FJzIQtzDn1meBVLAz"
+                    }
+                },
+                {
+                    "ref1":
+                    {
+                        "name": "Bob",
+                        "i": "ECWJZFBtllh99fESUOrBvT3EtBujWtDKCmyzDAXWhYmf"
+                    }
+                }
+            ]
+        }
+    }
+
+    sad = serder.sad
+
+    path = '-'
+    pather = Pather(path=path)
+    assert pather.qb64 == '6AABAAA-'
+    assert pather.resolve(sad=sad) == sad
+
+    path = '-a-personal'
+    pather = Pather(path=path)
+    assert pather.qb64 == '4AADA-a-personal'
+    assert pather.resolve(sad=sad) == {'name': 'John Doe', 'home': 'Atlanta'}
+
+    path = '-5-3'
+    pather = Pather(path=path)
+    assert pather.qb64 == '4AAB-5-3'
+    assert pather.resolve(sad=sad) == {'name': 'John Doe', 'home': 'Atlanta'}
+
+    path = '-5-3-name'
+    pather = Pather(path=path)
+    assert pather.qb64 == '6AADAAA-5-3-name'
+    assert pather.resolve(sad=sad) == 'John Doe'
+
+    path = '-a-personal-1'
+    pather = Pather(path=path)
+    assert pather.qb64 == '6AAEAAA-a-personal-1'
+    assert pather.resolve(sad=sad) == 'Atlanta'
+
+    path = '-a-p-1-0'
+    pather = Pather(path=path)
+    assert pather.qb64 == '4AAC-a-p-1-0'
+    assert pather.resolve(sad=sad) == {'name': 'Bob', 'i': 'ECWJZFBtllh99fESUOrBvT3EtBujWtDKCmyzDAXWhYmf'}
+
+    path = '-a-p-0-0-name'
+    pather = Pather(path=path)
+    assert pather.qb64 == '6AAEAAA-a-p-0-0-name'
+    assert pather.resolve(sad=sad) == 'Amy'
+
+    path = '-a-p-0-ref0-i'
+    pather = Pather(path=path)
+    assert pather.qb64 == '6AAEAAA-a-p-0-ref0-i'
+    assert pather.resolve(sad=sad) == 'ECmiMVHTfZIjhA_rovnfx73T3G_FJzIQtzDn1meBVLAz'
 
 
     """Done Test"""
+
+
+
 
 if __name__ == "__main__":
     test_cesr_examples()
