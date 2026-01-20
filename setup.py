4fe1249e6b5f22cfb52866bf96a71c34514ed002
Samuel M Smith <sam@samuelsmith.org>
2026-01-19 19:23:44 -0700

    updated python required to 3.14.2

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.13.2',
+    python_requires='>=3.14.2',
     install_requires=[
                         'lmdb>=1.7.3',
                         'pysodium>=0.7.18',
