9bf14cf6b1d4a0d9bbde5e30745e5fbb3e85748d
Kevin Griffin <griffin.kev@gmail.com>
2026-01-31 14:17:23 -0500

    Add kli contacts commands
    Cherry-picked contact commands from GLEIF-IT/keripy (ba068228).
    Adds add, delete, find, get, query, and rename subcommands for kli contacts.

---
+++ src/keri/app/cli/commands/contacts/query.py
@@ -0,0 +1,135 @@
+# -*- encoding: utf-8 -*-
+"""
+KERI
+keri.kli.commands.contacts.query module
+
+"""
+import argparse
+import datetime
+import json
+
+from keri import help
+from hio.base import doing
+from hio.help import decking
+
+from keri.app import connecting, habbing, indirecting, querying
+from keri.app.cli.common import existing, displaying
+from keri.help import helping
+
+logger = help.ogler.getLogger()
+
+parser = argparse.ArgumentParser(description='Query witnesses for latest key state of a contact')
+parser.set_defaults(handler=lambda args: handler(args),
+                    transferable=True)
+parser.add_argument('--name', '-n', help='keystore name and file location of KERI keystore', required=True)
+parser.add_argument('--base', '-b', help='additional optional prefix to file location of KERI keystore',
+                    required=False, default="")
+parser.add_argument('--passcode', '-p', help='22 character encryption passcode for keystore (is not saved)',
+                    dest="bran", default=None)  # passcode => bran
+parser.add_argument('--alias', '-a', help='alias of your local identifier for querying', required=True)
+parser.add_argument('--contact-aid', dest='contact_aid', help='contact AID prefix to query',
+                    required=False, default=None)
+parser.add_argument('--contact-alias', dest='contact_alias', help='contact alias to lookup',
+                    required=False, default=None)
+
+
+def handler(args):
+    """ command line method for querying contact key state from witnesses
+
+    Parameters:
+        args(Namespace): parse args namespace object
+
+    """
+    name = args.name
+    base = args.base
+    bran = args.bran
+    alias = args.alias
+    contact_aid = args.contact_aid
+    contact_alias = args.contact_alias
+
+    if contact_aid is None and contact_alias is None:
+        print("Either --contact-aid or --contact-alias is required")
+        return []
+
+    queryDoer = ContactQueryDoer(name=name, base=base, bran=bran, alias=alias,
+                                 contact_aid=contact_aid, contact_alias=contact_alias)
+    return [queryDoer]
+
+
+class ContactQueryDoer(doing.DoDoer):
+    """ DoDoer for querying contact key state from witnesses """
+
+    def __init__(self, name, base, bran, alias, contact_aid, contact_alias):
+        self.hby = existing.setupHby(name=name, base=base, bran=bran)
+        self.hbyDoer = habbing.HaberyDoer(habery=self.hby)
+        self.alias = alias
+        self.contact_aid = contact_aid
+        self.contact_alias = contact_alias
+
+        self.mbd = indirecting.MailboxDirector(hby=self.hby, topics=["/replay", "/receipt", "/reply"])
+
+        doers = [self.hbyDoer, self.mbd, doing.doify(self.queryDo)]
+        super(ContactQueryDoer, self).__init__(doers=doers)
+
+    def queryDo(self, tymth, tock=0.0):
+        """ Query witnesses for contact key state
+
+        Parameters:
+            tymth (function): injected function wrapper closure returned by .tymen() of
+                Tymist instance. Calling tymth() returns associated Tymist .tyme.
+            tock (float): injected initial tock value
+
+        Returns:  doifiable Doist compatible generator method
+        """
+        self.wind(tymth)
+        self.tock = tock
+        _ = (yield self.tock)
+
+        hab = self.hby.habByName(self.alias)
+        if hab is None:
+            print(f"Local identifier '{self.alias}' not found")
+            self.remove([self.hbyDoer, self.mbd])
+            return
+
+        org = connecting.Organizer(hby=self.hby)
+
+        pre = None
+        if self.contact_aid:
+            pre = self.contact_aid
+            contact = org.get(pre)
+        elif self.contact_alias:
+            contacts = org.find('alias', f"^{self.contact_alias}$")
+            if len(contacts) == 0:
+                print(f"Contact with alias '{self.contact_alias}' not found")
+                self.remove([self.hbyDoer, self.mbd])
+                return
+            if len(contacts) > 1:
+                print(f"Multiple contacts match alias '{self.contact_alias}'")
+                self.remove([self.hbyDoer, self.mbd])
+                return
+            contact = contacts[0]
+            pre = contact['id']
+
+        if pre is None:
+            print("Contact not found")
+            self.remove([self.hbyDoer, self.mbd])
+            return
+
+        doer = querying.QueryDoer(hby=self.hby, hab=hab, pre=pre, kvy=self.mbd.kvy)
+        self.extend([doer])
+
+        end = helping.nowUTC() + datetime.timedelta(seconds=10)
+        while helping.nowUTC() < end:
+            if doer.done:
+                break
+            yield 1.0
+
+        self.remove([doer])
+
+        displaying.printExternal(self.hby, pre)
+
+        contact = org.get(pre)
+        if contact:
+            print(json.dumps(contact, indent=2))
+
+        self.remove([self.hbyDoer, self.mbd])
---
+++ src/keri/app/cli/commands/contacts/rename.py
@@ -0,0 +1,84 @@
+# -*- encoding: utf-8 -*-
+"""
+KERI
+keri.kli.commands.contacts.rename module
+
+"""
+import argparse
+
+from keri import help
+from hio.base import doing
+
+from keri.app import connecting
+from keri.app.cli.common import existing
+from keri.kering import ConfigurationError
+
+logger = help.ogler.getLogger()
+
+parser = argparse.ArgumentParser(description='Rename a contact alias')
+parser.set_defaults(handler=lambda args: handler(args),
+                    transferable=True)
+parser.add_argument('--name', '-n', help='keystore name and file location of KERI keystore', required=True)
+parser.add_argument('--base', '-b', help='additional optional prefix to file location of KERI keystore',
+                    required=False, default="")
+parser.add_argument('--passcode', '-p', help='22 character encryption passcode for keystore (is not saved)',
+                    dest="bran", default=None)  # passcode => bran
+parser.add_argument('--aid', '-a', help='contact AID prefix', required=False, default=None)
+parser.add_argument('--old-alias', dest='old_alias', help='current alias to lookup', required=False, default=None)
+parser.add_argument('--alias', help='new alias to set', required=True)
+
+
+def handler(args):
+    kwa = dict(args=args)
+    return [doing.doify(rename, **kwa)]
+
+
+def rename(tymth, tock=0.0, **opts):
+    """ Command line handler for renaming a contact alias
+
+    """
+    _ = (yield tock)
+    args = opts["args"]
+    name = args.name
+    base = args.base
+    bran = args.bran
+    aid = args.aid
+    old_alias = args.old_alias
+    new_alias = args.alias
+
+    if aid is None and old_alias is None:
+        print("Either --aid or --old-alias is required")
+        return -1
+
+    try:
+        with existing.existingHby(name=name, base=base, bran=bran) as hby:
+            org = connecting.Organizer(hby=hby)
+
+            contact = None
+            pre = None
+
+            if aid:
+                pre = aid
+                contact = org.get(aid)
+            elif old_alias:
+                contacts = org.find('alias', f"^{old_alias}$")  # Exact match
+                if len(contacts) == 0:
+                    print(f"Contact with alias '{old_alias}' not found")
+                    return -1
+                if len(contacts) > 1:
+                    print(f"Multiple contacts match alias '{old_alias}'")
+                    return -1
+                contact = contacts[0]
+                pre = contact['id']
+
+            if contact is None:
+                print("Contact not found")
+                return -1
+
+            old_alias_display = contact.get('alias', '<none>')
+            org.set(pre, 'alias', new_alias)
+            print(f"Renamed contact {pre} from '{old_alias_display}' to '{new_alias}'")
+
+    except ConfigurationError as e:
+        print(f"identifier prefix for {name} does not exist, incept must be run first")
+        return -1
