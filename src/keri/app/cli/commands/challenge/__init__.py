6953a1d8d43f2cb336990039ad4ccd1d9875afdc
Alec Forston <alec.s.forston@gmail.com>
2026-02-27 12:49:56 -0700

    add imports

---
+++ src/keri/app/cli/commands/challenge/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+KERI
+keri.app.cli.commands.challenge Package
+
+"""
+
+from .generate import generate, generateWords
+from .respond import RespondDoer
+from .verify import VerifyDoer
