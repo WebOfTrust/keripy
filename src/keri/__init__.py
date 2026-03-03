8bc44c8a83629e1a7fe29e932e5e481c56cfa8e8
Alec Forston <alec.s.forston@gmail.com>
2026-03-03 09:21:30 -0700

    add imports

--- src/keri/__init__.py
+++ src/keri/__init__.py
@@ -8,4 +8,6 @@ from .kering import (Vrsn_1_0, Vrsn_2_0, Kinds, Protocols, Colds, Ilks, sniff,
                      SerializeError, DeserializeError, InvalidValueError,
                      MissingSignatureError, ShortageError, EmptyMaterialError,
                      RawMaterialError, InvalidCodeError, InvalidSizeError,
+                     ConfigurationError, MissingEntryError, MissingAnchorError,
+                     TraitCodex,
                      Roles, Schemes, VER1FULLSPAN)
