72437d0dc4df70c43959e71fe7da05af87300e82
Kent Bull <kent@kentbull.com>
2026-06-25 11:16:28 -0600

    feat: backport kli vc schema import from main This is the first of a sequence of PRs for the export and import behavior.

---
+++ src/keri/app/cli/commands/vc/schema/import/_index.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+"""
+KERI
+keri.kli.commands.vc.schema.import module
+
+Expose `kli vc schema import` while keeping the implementation in import_.py.
+"""
+
+from keri.app.cli.commands.vc.schema import import_
+
+parser = import_.importParser
