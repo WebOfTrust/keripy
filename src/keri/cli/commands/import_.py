ec63753b556c2afc0226e5508de452bcd202e067
ryan-hansen <ryan.hansen@usa.net>
2026-07-16 16:47:29 -0600

    Roll back an overly greedy search and replace.

--- src/keri/cli/commands/import_.py
+++ src/keri/cli/commands/import_.py
@@ -11,6 +11,7 @@ from hio.base import doing
 
 from ..common import Parsery, setupHby
 
+from ...kering import Vrsn_1_0
 from ...app import HaberyDoer
 from ...core import Parser
 
@@ -64,7 +65,8 @@ class ImportDoer(doing.DoDoer):
 
         with open(self.file, 'rb') as f:
             ims = f.read()
-            Parser(kvy=self.hby.kvy, rvy=self.hby.rvy, local=False).parse(ims=ims)
+            Parser(kvy=self.hby.kvy, rvy=self.hby.rvy, local=False,
+                   version=Vrsn_1_0).parse(ims=ims)
             self.hby.kvy.processEscrows()
 
         self.exit()
