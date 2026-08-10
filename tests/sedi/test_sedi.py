94976454ef7c463c9f696246bb25fd5a8e7c9688
Samuel M Smith <sam@samuelsmith.org>
2026-08-10 12:05:54 -0600

    added test scaffolding for notional SEDI credentials

---
+++ tests/sedi/test_sedi.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+tests.sedi.test_sedi module
+
+"""
+import json
+import os
+from base64 import urlsafe_b64encode as encodeB64
+from base64 import urlsafe_b64decode as decodeB64
+
+import pytest
+
+from jsonschema import Draft202012Validator
+
+from keri import Vrsn_2_0, Kinds, Protocols, Ilks
+
+
+def test_core_identity():
+    """Test core identity recipt and entitlement """
+
+    """Done Test"""
+
+
+
+if __name__ == "__main__":
+    test_core_identity()
